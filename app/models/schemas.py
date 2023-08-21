5110c5d421d957e70c9789412f826ab96d9115f1
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-08-21 11:00:07 -0600

    feat: add pydantic models

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,1 +1,23 @@
 """Defines the pydantic models"""
+
+import enum
+from typing import Optional
+import pydantic
+
+
+class LevelOfConfidence(enum.Enum):
+    LEVEL_1 = "Debo empezar a aprender o desarrollar"
+    LEVEL_2 = "Estoy aprendiendo o desarrollando"
+    LEVEL_3 = "Tengo confianza"
+
+
+class Skill(pydantic.BaseModel):
+    skill_name: str
+    level_of_confidence: LevelOfConfidence
+
+
+class PlaceWithGreaterInterest(pydantic.BaseModel):
+    place_name: str
+    website_link: pydantic.HttpUrl
+    job_postings_link: Optional[pydantic.HttpUrl]
+    linkedin_link: Optional[pydantic.HttpUrl]
