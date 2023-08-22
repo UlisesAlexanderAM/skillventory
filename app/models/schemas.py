d5b71813caf9ce2bf960234452c5090e6685afd9
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-08-21 20:29:38 -0600

    docs(schemas): add docstrings

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -6,17 +6,23 @@ import pydantic
 
 
 class LevelOfConfidence(enum.Enum):
+    """Enum of the level of confidence"""
+
     LEVEL_1 = "Debo empezar a aprender o desarrollar"
     LEVEL_2 = "Estoy aprendiendo o desarrollando"
     LEVEL_3 = "Tengo confianza"
 
 
 class Skill(pydantic.BaseModel):
+    """Model of the skill/knowledge/competence"""
+
     skill_name: str
     level_of_confidence: LevelOfConfidence
 
 
 class PlaceWithGreaterInterest(pydantic.BaseModel):
+    """Model of the place with greater interest"""
+
     place_name: str
     website_link: pydantic.HttpUrl
     job_postings_link: Optional[pydantic.HttpUrl]
