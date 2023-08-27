61a424dc42c533190103bb34af49d56913ccedbd
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-08-26 19:27:54 -0600

    feat: add a function that create a skill in the database

--- app/data/crud.py
+++ app/data/crud.py
@@ -1,1 +1,16 @@
 """CRUD functions"""
+import sqlalchemy
+from sqlalchemy import orm
+
+from app.models import schemas, models
+
+
+def get_skill(skill_name: str):
+    pass
+
+
+def create_skill(session: orm.Session, skill: schemas.Skill):
+    skill_db = models.Skill(**skill.model_dump())
+    session.add(skill_db)
+    session.commit()
+    session.refresh(skill_db)
