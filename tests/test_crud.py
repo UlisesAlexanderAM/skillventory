2d3694b51aef23bf02f4f2346035443191da81b7
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-08-28 12:18:49 -0600

    test: add tests for the function `get_skills`
    - One test check if a database without skills returns an empty sequence.
    - One test check if a database with one skill return a sequence with one skill and check the values of said skill.
    - One test check if a database with 2 skills return a sequence of 2 skills  and check the values of the skills.

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -35,3 +35,35 @@ def test_create_skill(get_db_session, setup_db):
     skill_db = get_db_session.scalars(stmt).one_or_none()
     assert skill_db.skill_name == "python"
     assert skill_db.level_of_confidence == schemas.LevelOfConfidence.LEVEL_1
+
+
+class TestGetSkills:
+    skill_1 = schemas.SkillBase(
+        skill_name="python", level_of_confidence=schemas.LevelOfConfidence.LEVEL_2
+    )
+    skill_2 = schemas.SkillBase(
+        skill_name="typescript",
+        level_of_confidence=schemas.LevelOfConfidence.LEVEL_1,
+    )
+
+    def test_get_zero_skills(self, get_db_session, setup_db):
+        skill = crud.get_skills(get_db_session)
+        assert skill == []
+        assert len(skill) == 0
+
+    def test_get_one_skill(self, get_db_session, setup_db):
+        crud.create_skill(session=get_db_session, skill=self.skill_1)
+        skills = crud.get_skills(get_db_session)
+        assert len(skills) == 1
+        assert skills[0].skill_name == "python"
+        assert skills[0].level_of_confidence == schemas.LevelOfConfidence.LEVEL_2
+
+    def test_get_multiple_skills(self, get_db_session, setup_db):
+        crud.create_skill(get_db_session, self.skill_1)
+        crud.create_skill(get_db_session, self.skill_2)
+        skills = crud.get_skills(get_db_session)
+        assert len(skills) == 2
+        assert skills[0].skill_name == "python"
+        assert skills[0].level_of_confidence == schemas.LevelOfConfidence.LEVEL_2
+        assert skills[1].skill_name == "typescript"
+        assert skills[1].level_of_confidence == schemas.LevelOfConfidence.LEVEL_1
