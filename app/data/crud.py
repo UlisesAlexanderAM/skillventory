bc8841c65a0bbf509ebfad798a9496165d03e74a
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-08-29 09:28:59 -0600

    feat: raises an exeption when trying to create an existing skill

--- app/data/crud.py
+++ app/data/crud.py
@@ -1,5 +1,6 @@
 """CRUD functions"""
 from collections.abc import Sequence
+import sqlite3
 import sqlalchemy
 from sqlalchemy import orm
 
@@ -7,6 +8,15 @@ from app.models import schemas, models
 
 
 def create_skill(session: orm.Session, skill: schemas.Skill):
+    if (
+        session.scalar(
+            sqlalchemy.select(models.Skill.skill_name).where(
+                models.Skill.skill_name == skill.skill_name
+            )
+        )
+        is not None
+    ):
+        raise sqlite3.IntegrityError("The skill already exist")
     skill_db = models.Skill(**skill.model_dump())
     session.add(skill_db)
     session.commit()
