fe4723b37cbe3151045dcb327039b188c20571b1
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-09-06 12:48:11 -0600

    feat: add function to use as dependencie in fastapi
    The function gets a database session object to be passed as a dependencie to the routes in fastapi

--- app/data/dependencies.py
+++ app/data/dependencies.py
@@ -1,1 +1,17 @@
 """Defines the dependencies used"""
+from sqlalchemy.orm import Session
+from collections.abc import Iterator
+from app.database import config
+
+
+def get_db_session() -> Iterator[Session]:
+    """Gets a database session object.
+
+    Yields:
+        db: The database session.
+    """
+    db: Session = config.LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
