99ddef33785b4b83aa1b17fe16df79646eb2ecd9
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-09-24 21:21:31 -0600

    feat: add funcionality for the HTTP method get of the route /skills

--- app/routers/skills.py
+++ app/routers/skills.py
@@ -1,15 +1,24 @@
 """Module that defines the routes related to skills/knowledge/competence."""
-import fastapi
-from fastapi import APIRouter
+from typing import Annotated
+import fastapi as fa
+from fastapi import APIRouter, status
+from collections.abc import Sequence
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from app.data import crud, dependencies as deps
+from app.models.type_aliases import skill_schema, skill_model, skill_base_schema
 
 
-router: APIRouter = fastapi.APIRouter(
+router: APIRouter = fa.APIRouter(
     prefix="/skills",
     tags=["Skills"],
     responses={404: {"description": "Skill not found"}},
 )
 
 
-# @routers.get("/", status_code=status.HTTP_200_OK, response_model=Sequence[skill_schema])
-# def get_skills(session: Session = Depends(deps.get_db_session)):
-#     pass
+@router.get("/", status_code=status.HTTP_200_OK, response_model=Sequence[skill_schema])
+def get_skills(
+    session: Annotated[Session, fa.Depends(deps.get_db_session)],
+) -> Sequence[skill_model]:
+    return crud.get_skills(session=session)
