3320eecf82e6a163e057a583b9616997fb900568
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-09-24 21:20:16 -0600

    test: add test for the HTTP method get of the route /skills

--- app/tests/test_skill_router.py
+++ app/tests/test_skill_router.py
@@ -1,9 +1,18 @@
-import fastapi
-from fastapi import testclient
+import pytest
+from fastapi import status, testclient
+from httpx import Response
 
 from app import main
-from app.data import dependencies
-from tests import conftest
 
-main.app.dependency_overrides[dependencies.get_db_session] = conftest.get_db_session
+client = testclient.TestClient(app=main.app)
 
+
+@pytest.mark.usefixtures("override_get_db_session")
+@pytest.mark.parametrize("num_skills", [0, 1, 2])
+def test_get_skills(num_skills: int, skills_json) -> None:
+    for _ in range(num_skills):
+        client.post("/skills", json=skills_json[_])
+    response: Response = client.get("/skills")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == num_skills
