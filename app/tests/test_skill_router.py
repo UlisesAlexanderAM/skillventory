9981c30bb1610ff9e087966576e229368363f47e
Ulises Alexander AM <ulises_alexander_am@proton.me>
2023-09-24 21:22:55 -0600

    test: add test for the HTTP method post of the route /skills

--- app/tests/test_skill_router.py
+++ app/tests/test_skill_router.py
@@ -16,3 +16,24 @@ def test_get_skills(num_skills: int, skills_json) -> None:
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()) == num_skills
+
+
+@pytest.mark.parametrize(
+    "expected_status_code,expected_json, num_skills",
+    [
+        (status.HTTP_201_CREATED, {"message": "Skill added successfully"}, 1),
+        (status.HTTP_409_CONFLICT, {"detail": "Skill already added"}, 2),
+    ],
+)
+def test_post_skill(expected_status_code, expected_json, num_skills, skill_1) -> None:
+    for _ in range(num_skills):
+        response = client.post(
+            "/skills",
+            json={
+                "skill_name": skill_1.skill_name,
+                "level_of_confidence": skill_1.level_of_confidence.value,
+            },
+        )
+
+    assert response.status_code == expected_status_code
+    assert response.json() == expected_json
